b5c16795509ddee9ed24527bf6d1263810b12d45
반성문써와 <52438080+DevKimYj@users.noreply.github.com>
2019-07-09 00:40:46 +0900

    프로젝트 생성

---
+++ workspace/CPPC/src/main/kotlin/com/u1/cppc/CppcApplication.kt
@@ -0,0 +1,11 @@
+package com.u1.cppc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CppcApplication
+
+fun main(args: Array<String>) {
+	runApplication<CppcApplication>(*args)
+}
