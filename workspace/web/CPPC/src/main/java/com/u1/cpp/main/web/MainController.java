cde97d0d7c29566c8f4a2f53b07b8b91a51a3ae9
반성문써와 <52438080+DevKimYj@users.noreply.github.com>
2019-07-14 21:33:54 +0900

    git연동 테스트

--- workspace/web/CPPC/src/main/java/com/u1/cpp/main/web/MainController.java
+++ workspace/web/CPPC/src/main/java/com/u1/cpp/main/web/MainController.java
@@ -1,7 +1,5 @@
 package com.u1.cpp.main.web;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -9,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
@@ -24,7 +21,7 @@ public class MainController {
 	 */
 	@RequestMapping(value = "/main.cpp")
 	public String main(Locale locale, Model model) {
-		
+		logger.debug("call /main.cpp");
 		return "main/main";
 	}
 	
