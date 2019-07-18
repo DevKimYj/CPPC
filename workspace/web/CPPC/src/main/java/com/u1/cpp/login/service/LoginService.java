d51d15532be968f50905064fa558dea5291b3c04
반성문써와 <52438080+DevKimYj@users.noreply.github.com>
2019-07-18 19:24:07 +0900

    프로젝트 업로드

---
+++ workspace/web/CPPC/src/main/java/com/u1/cpp/login/service/LoginService.java
@@ -0,0 +1,14 @@
+package com.u1.cpp.login.service;
+
+public interface LoginService {
+	/**
+	  * @Method Name : loginCheck
+	  * @작성일 : 2019. 7. 17.
+	  * @작성자 : Include
+	  * @변경이력 : 
+	  * @Method 설명 : 로그인 요청 검사
+	  * @return
+	  * @throws Exception
+	 */
+	UserVO loginCheck(UserVO userVo) throws Exception;
+}
---
+++ workspace/web/CPPC/src/main/java/com/u1/cpp/login/service/impl/LoginDAO.java
@@ -0,0 +1,15 @@
+package com.u1.cpp.login.service.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.u1.cpp.login.service.UserVO;
+
+@Repository("loginDAO")
+public class LoginDAO {
+
+	public UserVO loginCheck(UserVO userVo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
